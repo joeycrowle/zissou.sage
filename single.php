3923b7c1d623a0740155d2e8e0700d0cfd2834d9
joeycrowle <joeycrowle@gmail.com>
2019-06-10 10:27:04 -0700

    fonts / previews

--- single.php
+++ single.php
@@ -1,6 +1,8 @@
 <article >
   <?php
   get_template_part('templates/content-single', get_post_type());
-  include('templates/article-nav.php');
+
   ?>
 </article>
+
+<?php include('templates/article-nav.php'); ?>
--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,15 @@
 <footer class="content-info">
   <div class="container">
     <div class="row">
-      <div class="col">
+      <div class="col-4">
         <?php wp_nav_menu(array('theme_location'=>'pages_navigation')); ?>
+      </div>
+      <div class="col-4 icons">
+        <div class="social-icons">
+          <a href="#"><?php get_template_part('templates/social-facebook'); ?></a>
+          <a href="#"><?php get_template_part('templates/social-twitter'); ?></a>
+          <a href="#"><?php get_template_part('templates/social-instagram'); ?></a>
+        </div>
       </div>
     </div>
   </div>
