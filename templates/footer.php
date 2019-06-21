c90c194cde955f14966899a6436dbeb57bc46c4f
joeycrowle <joeycrowle@gmail.com>
2019-06-21 10:22:16 -0700

    subscribe form / block quote size / stuck header

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,7 @@
 <footer class="content-info">
   <div class="z-container">
     <div class="inner">
+
       <div class="row justify-content-center">
         <div class="col">
           <div class="row">
@@ -13,8 +14,27 @@
           </div>
         </div>
       </div>
-    </div>
 
+      <div class="row">
+        <div class="col">
+          <div class="subscribe-form">
+            <div class="subscribe-message">
+              <p class="bold">Join Our Mailinglist</p>
+            </div>
+            <div id="mc_embed_signup">
+              <form action="https://zissou.us20.list-manage.com/subscribe/post?u=184daa501e1faf9444694d810&amp;id=e8719a7aeb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="example@domain.com" required>
+                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_184daa501e1faf9444694d810_e8719a7aeb" tabindex="-1" value=""></div>
+                    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                  </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
   </div>
 </footer>
